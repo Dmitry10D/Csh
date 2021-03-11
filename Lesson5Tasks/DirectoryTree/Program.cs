07e61fe7e84881ef2fe76e8a2c52e0dd8d902c32
Dmitry10D <79482393+Dmitry10D@users.noreply.github.com>
2021-03-11 17:54:57 +0300

    Without additional tasks today

---
+++ Lesson5Tasks/DirectoryTree/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DirectoryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь каталога");
+            string input = Console.ReadLine();
+            string[] entries = Directory.GetFileSystemEntries(input, "*",SearchOption.AllDirectories);
+            File.WriteAllLines("TreeByCycle.txt", entries);
+
+
+            //for (int i = 0; i< entries.Length; i++)
+            //{
+            //    Console.WriteLine(entries[i]);
+
+            //}
+        }
+    }
+}
