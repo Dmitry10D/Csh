2beb37bbf8d4c8353067807b1b22fcff84f29cdd
Dmitry10D <79482393+Dmitry10D@users.noreply.github.com>
2021-03-15 10:01:40 +0300

    uploadTry3

---
+++ Lesson5Tasks/NumbersToBinary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace NumbersToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите числа через пробел");
+                string input = Console.ReadLine();
+                File.WriteAllBytes("setofbytes.bin", Parser(input));
+                byte []readed = File.ReadAllBytes("setofbytes.bin");//проверка записи в файл
+            }
+        }
+        static byte[] Parser(string input)//Метод переводит строку в массив byte
+        {
+            string[] numbers = input.Split(' ');
+            byte[] byte_array = new byte[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                byte_array[i] = Convert.ToByte(numbers[i]);
+            }
+            return byte_array;
+        }
+    }
+}
